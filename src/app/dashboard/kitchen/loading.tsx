9340a561acf647f2c08f0b701d64570c3b2435e0
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2023-04-02 21:16:38 -0300

    feat: OrderBoard Component add

--- src/app/dashboard/kitchen/loading.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center align-middle h-screen w-screen">
-      <h1>Loading</h1>
-    </div>
-  )
-}
---
+++ src/types/Order.ts
@@ -0,0 +1,14 @@
+export interface OrderProps {
+  _id: string;
+  table: string;
+  status: 'WAITING' | 'IN_PRODUCTION' | 'DONE';
+  products: Array<{
+    _id: string;
+    quantity: number;
+    product: {
+      name: string;
+      imagePath: string;
+      price: number;
+    }
+  }>
+}
