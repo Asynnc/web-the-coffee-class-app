f872a8ca3a8b9000299b6c4127daa6bfa5599ce7
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2023-04-30 14:19:23 -0300

    feat: update domains to search imagens on S3

--- next.config.js
+++ next.config.js
@@ -16,13 +16,18 @@ const nextConfig = {
   },
   images: {
     minimumCacheTTL: 60,
-    domains: ['https://imgur.com/', 'http://213.136.81.169:3001'],
+    domains: ['https://imgur.com/', 'http://213.136.81.169:3001/', 'the-coffee-class.s3.amazonaws.com'],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '3001',
         pathname: '/uploads/**',
+      }, {
+        protocol: 'https',
+        hostname: 'the-coffee-class.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
