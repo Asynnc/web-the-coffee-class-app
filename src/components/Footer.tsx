0f3b73f4ae737b3f1ad20e343178de36c158e3a4
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2023-05-07 16:24:08 -0300

    fix: fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
 
   return (
     <footer
-      className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left absolute bottom-0 w-full max-w-4xl">
+      className="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left mt-24 bottom-0 w-full max-w-4xl">
       <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
         © {now.getFullYear()}:
         <a
