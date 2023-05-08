ac2c5002cb29e629df98d04f2658b035fc247b15
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2023-05-07 21:33:52 -0300

    feat: update orders page

--- src/app/dashboard/(kitchen)/orders/page.tsx
+++ src/app/dashboard/(kitchen)/orders/page.tsx
@@ -1,7 +1,9 @@
-import OrdersBoard from "@/components/OrderBoard";
+import OrdersBoard from "@/components/Orders/OrderBoard";
 import PageTitle from "@/components/PageTitle";
+import Spinner from "@/components/Spinners/Spinner";
 import { ordersMock } from "@/mocks/order";
 import { OrderProps } from "@/types/Order";
+import { Suspense } from "react";
 
 const getOrders = async (): Promise<OrderProps[] | any> => {
   try {
@@ -32,7 +34,9 @@ export default async function Orders() {
     <main className="prose prose-invert prose-headings:text-zinc-100 prose-headings:font-semibold prose-h1:text-3xl prose-h2:text-xl prose-h2:font-medium prose-p:text-zinc-300 prose-pre:bg-zinc-900 prose-pre:border prose-pre:border-white/10 prose-pre:rounded-2xl prose-a:text-cyan-400">
       <PageTitle title="Kitchen" description="Manage the status of all orders here." />
       <div aria-label="Orders Board" className="flex items-center justify-center w-full">
-        <OrdersBoard ordersWaiting={waiting} ordersProduction={production} ordersDone={done} />
+        <Suspense fallback={<Spinner />}>
+          <OrdersBoard ordersWaiting={waiting} ordersProduction={production} ordersDone={done} />
+        </Suspense>
       </div>
     </main>
   )
