3ccf3d85c608831cefc68cdd4776be2f2217d485
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2023-05-15 16:17:21 -0300

    fix: anon function

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,7 +10,7 @@ export function Sidebar() {
     <aside className="border-r border-white/10 w-80 fixed lest-0 top-0 bottom-0 p-6 hidden lg:block">
       <Image src={LogoIMG} alt="The Coffee Class Logo" className="-mt-4 cursor-pointer" role='presentation' />
       <Suspense fallback={<Spinner />}>
-        <NavLinks />
+        <NavLinks onClick={() => console.log('ok')} />
       </Suspense>
     </aside>
   )
